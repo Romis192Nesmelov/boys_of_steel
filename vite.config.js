c605a701673521e57767d145bc114bce02ef396b
Romis <romis@nesmelov.com>
2025-04-13 07:46:47 +0300

    resolved additional js and css

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,14 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/styles.css',
+                'resources/css/owl.carousel.min.css',
+                'resources/css/jquery.fancybox.min.css',
+                'resources/css/icons/icomoon/styles.css',
                 'resources/js/app.js',
-                'resources/js/main.js',
+                'resources/js/owl.carousel.js',
+                'resources/js/jquery.fancybox.min.js',
+                'resources/js/main.js'
             ],
             refresh: true,
         }),
