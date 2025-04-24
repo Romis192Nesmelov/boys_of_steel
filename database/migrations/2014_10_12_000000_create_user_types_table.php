acea7f152287316556c7735e70ef75716887d801
Romis <romis@nesmelov.com>
2025-04-24 07:44:50 +0300

    try to fix migrations

--- database/migrations/2014_10_12_000000_create_user_types_table.php
+++ database/migrations/2014_10_12_000000_create_user_types_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('user_types', function (Blueprint $table) {
             $table->id();
-            $table->string('name',191);
+            $table->string('name');
             $table->timestamps();
         });
     }
--- database/migrations/2014_10_12_000001_create_cities_table.php
+++ database/migrations/2014_10_12_000001_create_cities_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('cities', function (Blueprint $table) {
             $table->id();
-            $table->string('name',191);
+            $table->string('name');
             $table->timestamps();
         });
     }
