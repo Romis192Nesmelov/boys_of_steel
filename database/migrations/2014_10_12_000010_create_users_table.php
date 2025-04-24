348087f2b18b920cc10744c6370b284f027482c4
Romis <romis@nesmelov.com>
2025-04-24 07:20:10 +0300

    try to fix migrations

--- database/migrations/2014_10_12_000010_create_users_table.php
+++ database/migrations/2014_10_12_000010_create_users_table.php
@@ -16,11 +16,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name',255);
+            $table->string('email',255)->unique();
             $table->string('phone',20)->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password',255);
             $table->foreignIdFor(UserType::class)->nullable()->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignIdFor(City::class)->nullable()->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignIdFor(Team::class)->nullable()->constrained()->cascadeOnUpdate()->cascadeOnDelete();
