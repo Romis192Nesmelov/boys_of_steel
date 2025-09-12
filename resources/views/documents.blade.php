1406012cd6d8cadb58fb6c739ee1990906680b7c
Romis <romis@nesmelov.com>
2025-09-12 17:23:08 +0300

    disable some docs

--- resources/views/documents.blade.php
+++ resources/views/documents.blade.php
@@ -5,11 +5,11 @@
         @include('partials.head1',['head' => $breadcrumbs[0]['name']])
 
         <div class="max-w-7xl mx-auto py-3">
-            <div class="grid grid-cols-1 lg:grid-cols-3 gap-6 px-6">
+            <div class="grid grid-cols-1 lg:grid-cols-1 gap-6 px-6">
                 @each('partials.docs.doc',[
                     'certificate_of_state_registration',
-                    'extract_from_the_USRLE',
-                    'SHL_position'
+                    //'extract_from_the_USRLE',
+                    //'SHL_position'
                 ],'doc')
             </div>
         </div>
