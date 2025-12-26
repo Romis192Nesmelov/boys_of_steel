f607ad6b18c1e5e904062eb9b1ecd6674a4eb4c5
Romis <romis@nesmelov.com>
2025-12-26 14:06:03 +0300

    some fixes on documents page

--- resources/views/partials/docs/doc.blade.php
+++ resources/views/partials/docs/doc.blade.php
@@ -2,6 +2,6 @@
     <h2 class="mb-3 text-md lg:text-xl font-semibold text-center text-white">{{ $doc->name }}</h2>
     <img class="w-30 xs:w-full mr-0 border-2 border-gray-100 border-solid mb-3" src="{{ asset('storage/'.$doc->image) }}" />
     <a href="{{ asset('storage/'.$doc->doc) }}" target="_blank">
-        <button class="bg-gray-200 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">{{ __('Download').' PDF' }}</button>
+        <button class="bg-gray-200 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">{{ __('Download').' '.strtoupper(pathinfo($doc->doc)['extension']) }}</button>
     </a>
 </div>
